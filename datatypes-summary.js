6532e42cd321619efc7fe56bdc5312ae5885bda7
kartikaysaxena01 <144546155+kartikaysaxena01@users.noreply.github.com>
2024-10-26 18:50:03 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const bigNumber = 343538498328748789888n        //For Bigint
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects , Functions
+
+
+const heros = ["shaktiman" , "naagraj" , "doga"]
+
+
+// OBJECTS
+let myObj={
+    name: "kartik",
+    age: 22,
+}
+
+const myFunction = function()
+{
+    console.log("Hello World");
+    
+}
+
+console.log(typeof bigNumber)
